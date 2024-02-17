dca32e266d8e499b4bc6568f8138a1b008260908
larrymiami <larrymiami94@gmail.com>
2024-02-17 13:18:14 +0300

    fix custom theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { Inter } from "next/font/google";
-import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
+import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
 import theme from "@/theme";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -9,11 +9,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <MuiThemeProvider
+        <CssVarsProvider
           theme={theme}
         >
           <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
-        </MuiThemeProvider>
+        </CssVarsProvider>
       </body>
     </html >
   );
